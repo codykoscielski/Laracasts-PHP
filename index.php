aac19349e6f23c7f45339b4cde1c23373ae9cdf0
Cody Koscielski <codykoscielski@gmail.com>
2023-05-13 14:43:06 -0400

    Setup a PDO for a MySQL Connection and query the database

--- index.php
+++ index.php
@@ -2,3 +2,18 @@
 
 require 'functions.php';
 require 'router.php';
+
+//Connect to the database
+$dns = 'mysql:host=localhost;port=3306;dbname=php-demo;charset=utf8mb4';
+
+$pdo = new PDO($dns, 'root', 'root');
+
+$statement = $pdo->prepare('SELECT * FROM posts');
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>{$post['Title']}</li>";
+}
