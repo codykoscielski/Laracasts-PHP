4623f391936431274be62565dcdf83c8c0c9424e
Cody Koscielski <codykoscielski@gmail.com>
2023-05-12 22:10:45 -0400

    Started to setup routing/controllers

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php
 
-$books = [
-        [
-            'name' => 'Book name',
-            'author' => 'Author Name',
-            'releaseYear' => 1968,
-            'purchaseURL' => 'some URL'
-        ],
-                [
-            'name' => 'Book name 2',
-            'author' => 'Author Name 2',
-            'releaseYear' => 1969,
-            'purchaseURL' => 'some URL 2'
-        ]
-    ];
+    $heading = 'Home';
 
-    $filteredBooks = array_filter($books, function($book) {
-        return $book['releaseYear'] >= 1950 && $book['releaseYear'] <= 2002;
-    });
-
-    require "index.view.php";
+    require "views/index.view.php";
